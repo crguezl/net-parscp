80ffdd3cdb02bdedc64e82fea20b6a239cc6ace5
casiano.rodriguez.leon <casiano.rodriguez.leon@4c777606-195d-11de-ac9f-1d9250355bca>
2009-03-26 19:22:14 +0000

    doc

--- trunk/lib/Net/ParSCP.pm
+++ trunk/lib/Net/ParSCP.pm
@@ -127,10 +127,19 @@ Usage:
 
 Cluster expressions like:
 
-  $0 file 'beo-europa:/tmp'
-  $0 -s '-q' gmsh.pl  beowulf+orion:/tmp europa:/tmp/
+  $0 file 'cluster1-machine2:/tmp'
+  $0 file 'cluster1-machine2:/tmp/file_@=.txt'
+  $0 -s '-v' dir/  cluster1+cluster2:/tmp cluster3:/scratch/
 
-are accepted
+are accepted. The macro C<@=> inside a path expands to the name of the machine.
+To transfer several files protect them with quotes:
+
+  $0 'file1 machine3:file2' cluster1%cluster2:  
+
+it transfer file1 in the local machine and file2 in machine3 
+to the machines in the simetric difference of clusters cluster1 
+and cluster2.
+
 Valid options:
 
  --configfile file : Configuration file
