4e4ad063d14b499dd765980e81b94546e3fbf391
casiano.rodriguez.leon <casiano.rodriguez.leon@4c777606-195d-11de-ac9f-1d9250355bca>
2009-06-25 09:03:42 +0000

    preparing for 0.15

--- trunk/local.preamble.pl
+++ trunk/local.preamble.pl
@@ -2,15 +2,15 @@
 
 # Redefine them if needed
 #our $makebuilder = 'Makefile:PL';
-#our $build = 'make';
+our $build = 'touch Makefile; make';
 #our $makebuilder_arg = ''; # s.t. like INSTALL_BASE=~/personalmodules
 #our $build_arg = '';       # arguments for "make"/"Build"
 
 #our $build_test_arg = 'TEST_VERBOSE=1';
 
 # This code will be executed in the remote servers
-our %preamble = (
-  beowulf => q{ },
-  orion   => q{ system(q{touch Makefile }) }
-);
+#our %preamble = (
+#  beowulf => q{ },
+#  orion   => q{ }) }
+#);
 
